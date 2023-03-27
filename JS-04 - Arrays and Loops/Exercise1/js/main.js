888353a0e09619edf0e68c19bf4020b32f3c2954
Sorck <juanluis021001@gmail.com>
2023-03-27 16:42:44 -0600

    Se crea la estructura para los 3 ejercicios y se termina el ejercicio #1 comprobando resultados en consola

---
+++ JS-04 - Arrays and Loops/Exercise1/js/main.js
@@ -0,0 +1,21 @@
+//Inicio Exercise 1
+let bank = ["Sofía", "David", "Juan"]
+console.log("**********PART1**********");
+console.log(bank.length);
+console.log(bank); 
+// Part 1 of exercise
+
+console.log("**********PART2**********");
+bank.push("Sara", "Agustin")
+console.log(bank.shift());
+console.log(bank.length);
+console.log(bank);
+// part 2 of exercise
+
+console.log("**********PART3**********");
+console.log(bank.shift());
+bank.unshift("Renata")
+bank.push("Elena")
+console.log(bank.length);
+console.log(bank);
+//Part 3 of exercise
